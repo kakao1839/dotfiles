70feacabad0c481d5e7e2418ee1545f495398ed5
gou <kawachi20018@gmail.com>
2023-03-16 12:51:11 +0900

    change

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -2,24 +2,13 @@
 
 echo "Please install NerdFont"
 echo "https://www.nerdfonts.com"
-### Create a symbolic link ###
-echo "================================="
-echo "Create a symbolic link"
-echo "================================="
-DOT_FILES=(.zshrc .config .bashrc .bash_profile )
-
-for file in ${DOT_FILES[@]}
-do
-    ln -s $HOME/dotfiles/$file $HOME/$file
-done
-
 ### install packages ###
 echo "================================="
 echo "install packages"
 echo "================================="
 yes | sudo apt update
 yes | sudo apt install build-essential curl file git
-
+sleep 2
 ### install LinuxBrew ###
 echo "================================="
 echo "Start setting up LinuxBrew"
@@ -33,6 +22,9 @@ test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew
 
 # echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >> ~/.zshrc
 
+echo -e "\n"
+
+sleep 2
 echo "================================="
 echo "Setting zsh"
 echo "================================="
@@ -43,12 +35,34 @@ chsh -s /usr/bin/zsh $USER
 brew install zplug
 ln -s /home/linuxbrew/.linuxbrew/opt/zplug ~/.zplug
 
+sleep 2
 ### Starship ###
 echo "================================="
 echo "Setting Starship"
 echo "================================="
 
-curl -sS https://starship.rs/install.sh | sh
+yes | curl -sS https://starship.rs/install.sh | sh
 
 ################################################
+brew install lsd
+
+sleep 2
+
+echo "================================="
+echo "Create Symbolic link"
+echo "================================="
+DOT_DIR="$HOME/dotfiles"
+for f in *;
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".gitignore" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+    [[ "$f" == "README.md" ]] && continue
+    [[ "$f" == "./bin" ]] && continue
+
+    ln -snf $DOT_DIR/"$f" $HOME/".$f"
+    echo "Installed .$f"
+done
+
 echo END
+exec $SHELL -l
